a1252a4130e64f101e679dbb3a9dfeed699433da
Sam Rabeeh <srabeeh@larci.com>
2016-03-10 21:57:42 -0700

    Initial commit, created basic QR code capture and code metadata extraction to text label

--- My QReader/ViewController.swift
+++ My QReader/ViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var captureSession: AVCaptureSession?
+    
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    var qrCodeFrameView: UIView?
+    
+    
+    @IBOutlet weak var codeMessageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,74 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func scanCode(sender: AnyObject) {
+        
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        var error: NSError?
+        let input: AnyObject!
+        
+        do {
+            input = try AVCaptureDeviceInput(device: captureDevice)
+        }
+        catch let error1 as NSError {
+            error = error1
+            input = nil
+        }
+        
+        if (error != nil) {
+            print("\(error?.localizedDescription)")
+            return
+        }
+        
+        captureSession = AVCaptureSession()
+        
+        captureSession?.addInput(input as! AVCaptureInput)
+        
+        let captureMetadataOutput = AVCaptureMetadataOutput()
+        captureSession?.addOutput(captureMetadataOutput)
+        
+        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        
+        // To Do: on landscape layout must redraw bounds
+        
+        view.layer.addSublayer(videoPreviewLayer!)
+        
+        captureSession?.startRunning()
+        
+        view.bringSubviewToFront(codeMessageLabel)
+        
+        qrCodeFrameView = UIView()
+        qrCodeFrameView?.layer.borderColor = UIColor.greenColor().CGColor
+        qrCodeFrameView?.layer.borderWidth = 2
+        view.addSubview(qrCodeFrameView!)
+        view.bringSubviewToFront(qrCodeFrameView!)
+        
+    }
 
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRectZero
+            codeMessageLabel.text = "No QR Code Detected"
+            return
+        }
+        
+        let metadataObj = metadataObjects [0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObjectTypeQRCode {
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObjectForMetadataObject(metadataObj as AVMetadataMachineReadableCodeObject) as! AVMetadataMachineReadableCodeObject
+            qrCodeFrameView?.frame = barCodeObject.bounds
+            
+            if metadataObj.stringValue != nil {
+                codeMessageLabel.text = metadataObj.stringValue
+            }
+        }
+    }
 }
 
